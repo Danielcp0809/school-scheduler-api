73f12ea161fb878f83c9516873ea1c5bd44f433f
Daniel Cordova <jdany0809@hotmail.com>
2023-10-19 06:20:25 -0500

    [Add] Environment added by using useValue

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeachersModule } from './modules/teachers/teachers.module';
 
-@Module({
+const environment = process.env.NODE_ENV || 'development'
+@Module({ 
   imports: [TeachersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'ENVIRONMENT',
+      useValue: environment,
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
