1e408c5b3cd1da7b8122bd8b97054feeedaa5aee
Daniel Cordova <jdany0809@hotmail.com>
2023-10-18 23:33:35 -0500

    [Add] Teachers validator applied

--- src/modules/teachers/teachers.service.ts
+++ src/modules/teachers/teachers.service.ts
@@ -1,10 +1,13 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTeacher } from './teachers.entity';
+import { teacherUpdateSchema } from 'src/pipes/validator/teacher.validator';
 
 @Injectable()
 export class TeachersService {
     
     updateTeacher(id: string, body: Partial<CreateTeacher>) {
+        const { error } = teacherUpdateSchema.validate(body);
+        if(error) throw new BadRequestException('Validation failed: ' + error.message);
         const teacher = false;
         if(!teacher) throw new NotFoundException('Teacher not found')
         return {
