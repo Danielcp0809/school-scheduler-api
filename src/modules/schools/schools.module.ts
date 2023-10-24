1f950308486129a1519eda99da0bf1b163ba5bdc
Daniel Cordova <jdany0809@hotmail.com>
2023-10-23 20:48:04 -0500

    [Update] Schools and users modules imports

--- src/modules/schools/schools.module.ts
+++ src/modules/schools/schools.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { School } from 'src/models/schools.entity';
+import { Teacher } from 'src/models/teachers.entity';
+import { User } from 'src/models/users.entity';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([School, User, Teacher])],
+})
 export class SchoolsModule {}
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Credentials } from '../../models/credentials.entity';
+import { Teacher } from 'src/models/teachers.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Credentials])],
+  imports: [TypeOrmModule.forFeature([Credentials, Teacher])],
   controllers: [UsersController]
 })
 export class UsersModule {}
