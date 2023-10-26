c11b6f5d8e52e8db713552adbd7379503e90bbd7
Daniel Cordova <jdany0809@hotmail.com>
2023-10-26 18:39:44 -0500

    [Add] Api bearer token guard v1

--- src/modules/teachers/controllers/teachers.controller.ts
+++ src/modules/teachers/controllers/teachers.controller.ts
@@ -1,17 +1,20 @@
-import { Body, Controller, Get, Param, Post, Put, Query, UsePipes } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query, SetMetadata, UseGuards, UsePipes } from '@nestjs/common';
 import { TeachersService } from '../services/teachers.service';
 import { CreateTeacherDto, FiltersTeachersDto, UpdateTeacherDto } from 'src/validators/teachers.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { IsUUID } from 'class-validator';
 import { ValidatePayloadExistsPipe } from 'src/pipes/payload-exist/payload-exist.pipe';
+import { ApiKeyGuard } from 'src/auth/guards/api-key.guard';
+import { Public } from 'src/auth/decorators/public.decorator';
 
-@ApiTags('Teacher')
+@ApiTags('Teacher') 
+@UseGuards(ApiKeyGuard) // Makes all endpoints private by default
 @Controller('teachers')
 export class TeachersController {
 
     constructor(private teacherService: TeachersService){}
 
     @Get()
+    @Public() // Make this endpoint public
     @ApiOperation({ summary: 'Get the list of a teachers' })
     getTeachers(@Query() params: FiltersTeachersDto) {
         return this.teacherService.getAllTeachers(params);
